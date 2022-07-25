a91d060d4f1c5a3800f878a8f7f27158e9a4123e
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-07-25 22:39:12 +0900

    Baekjoon2750문제풀이

---
+++ Beakjoon2750/Beakjoon2750/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beakjoon2750
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linecount = int.Parse(Console.ReadLine());
+            int[] data = new int[linecount];
+            for (int i = 0; i < linecount; i++)
+            {
+                data[i] = (int.Parse(Console.ReadLine()));
+            }
+            bool sortcheck = false;
+            while (true)
+            {
+                sortcheck = false;
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (i+1<data.Length)
+                    {
+                        if (data[i] > data[i+1])
+                        {
+                            int temp = data[i];
+                            data[i] = data[i+1];
+                            data[i+1] = temp;
+                            sortcheck = true;
+                        }
+                    }
+                }
+                if (!sortcheck)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < linecount; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+    }
+}
